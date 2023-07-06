633d1f57f4f47a08a112047f46e957f202f5f229
Jaycee <1093033394@qq.com>
2023-07-06 23:13:52 +0800

    feat(reactivity/effect): 完善"effect"函数，调用后返回"runner"函数

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -7,7 +7,7 @@ class ReactiveEffect {
 
   run() {
     activeEffect = this
-    this._fn()
+    return this._fn()
   }
 }
 
@@ -42,6 +42,6 @@ let activeEffect
 export function effect(fn) {
   // fn
   const _effect = new ReactiveEffect(fn)
-
   _effect.run()
+  return _effect.run.bind(_effect)
 }
--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -18,4 +18,21 @@ describe('effect', () => {
     user.age++
     expect(nextAge).toBe(12)
   })
+
+  it('should return "runner" function when call "effect"', () => {
+    // 1. effect(fn) ==> function (runner) => fn ==> return
+    let foo = 10
+
+    const runner = effect(() => {
+      foo++
+      return 'foo'
+    })
+
+    expect(foo).toBe(11)
+
+    const res = runner()
+
+    expect(foo).toBe(12)
+    expect(res).toBe('foo')
+  })
 })
