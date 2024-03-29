cfbd75ac68e3e0405804b79266a6af3ac4dadfa2
Jaycee <1093033394@qq.com>
2024-03-29 18:59:14 +0800

    style: ci 工作流代码风格写法lint不通过，代码风格修改

--- src/reactivity/ref.ts
+++ src/reactivity/ref.ts
@@ -2,6 +2,7 @@ import { isTracking, track, trackEffects, triggerEffect } from './effect'
 
 class RefImpl {
   private _value: any
+
   public dep
 
   constructor(value) {
@@ -10,12 +11,16 @@ class RefImpl {
   }
 
   get value() {
-    isTracking() && trackEffects(this.dep)
+    if (isTracking()) {
+      trackEffects(this.dep)
+    }
     return this._value
   }
 
   set value(newValue) {
     // 步骤一定是先修改了 value 的值，然后才执行依赖触发流程
+
+    // newValue ==> this._value
     if (Object.is(newValue, this._value)) return
     this._value = newValue
     triggerEffect(this.dep)
