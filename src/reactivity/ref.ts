137f3ac9f16ca57216315bcc4d9c7112bd5bc832
Jaycee <1093033394@qq.com>
2024-03-29 19:58:32 +0800

    style: ref 对象类型响应式逻辑代码重构

--- src/reactivity/ref.ts
+++ src/reactivity/ref.ts
@@ -2,6 +2,13 @@ import { hasChanged, isObject } from '../shared'
 import { isTracking, trackEffects, triggerEffect } from './effect'
 import { reactive } from './reactive'
 
+// ref 和 reactive 的核心差别
+// ref 主要还是处理 单值(基本数据类型，如：1、true、"1")
+// get set
+// proxy ==> object,而proxy 主要针对的还是对象
+// 所以重写了 Ref ==> value，针对 value 实现了 get 和 set
+//
+
 class RefImpl {
   private _value: any
   private _rawValue: any
@@ -9,7 +16,7 @@ class RefImpl {
 
   constructor(value) {
     this._rawValue = value
-    this._value = isObject(value) ? reactive(value) : value
+    this._value = convert(value)
     // value is Object ==> reactive
     // 1. 看看 value 是不是 对象
     this.dep = new Set()
@@ -31,10 +38,14 @@ class RefImpl {
     // 所以需要拿到未被 reactive 包装过的值，因此声明一个 _rawValue 进行存储
     if (hasChanged(this._rawValue, newValue)) {
       this._rawValue = newValue
-      this._value = isObject(newValue) ? reactive(newValue) : newValue
+      this._value = convert(newValue)
       triggerEffect(this.dep)
     }
   }
+}
+
+function convert(value) {
+  return isObject(value) ? reactive(value) : value
 }
 
 function trackRefValue(ref) {
